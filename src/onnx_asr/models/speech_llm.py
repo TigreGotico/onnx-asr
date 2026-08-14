4f3bdb312797082b30192964404e3f35d5414e81
JarbasAi <jarbasai@mailfence.com>
2026-08-14 20:48:44 +0100

    fix: narrow SALM encoder length output type for mypy
    Rebased onto updated feat/speech-llm-qwen3-asr (relocked uv.lock + typed
    test helpers).

--- src/onnx_asr/models/speech_llm.py
+++ src/onnx_asr/models/speech_llm.py
@@ -30,7 +30,7 @@ import onnxruntime as rt
 from onnx_asr.asr import BaseAsr, Preprocessor, TimestampedResult
 from onnx_asr.models.whisper import bytes_to_unicode
 from onnx_asr.onnx import OnnxSessionOptions
-from onnx_asr.utils import is_float32_array
+from onnx_asr.utils import is_float32_array, is_int64_array
 
 NEG_INF = np.float32(-3.4028235e38)
 SPECIAL_TOKEN_PATTERN = re.compile(r"<\|.*\|>\Z|<(?:s|/s|unk|pad)>\Z")
@@ -148,6 +148,7 @@ class SpeechLlm(BaseAsr):
                 },
             )
             assert is_float32_array(audio_embeds)
+            assert is_int64_array(lens)
             return audio_embeds[:, : int(lens[0])]
         else:
             padded_frames, valid_indices, bias = self._audio_windows(feature_len)
